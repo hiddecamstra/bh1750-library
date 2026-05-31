12ab85f328347041a76b23da52292bdad882826f
Hidde Camstra <hiddecamstra@gmail.com>
2026-05-31 15:42:35 +0200

    Basic LED example made functional

--- bh1750-library/src/main.cpp
+++ bh1750-library/src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int ledPin = 13;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
